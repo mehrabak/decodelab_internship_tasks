e7306408d9c4d0b557f27b3f573027b74e1e5142
Mehrab <mehrabakareem167@gmail.com>
2026-07-29 04:10:50 +0500

    Refactor GameEngine and fix bugs
    Refactor GameEngine class to use maxNum for target number range, fix attempt initialization, and update method names for consistency.

--- Number_Guessing_game/GameEngine.java
+++ Number_Guessing_game/GameEngine.java
@@ -1,56 +1,44 @@
-import java.util.*;
 import java.util.Random;
 
-
-public class GameEngine{
+public class GameEngine {
 
     private final int targetNum;
     private final int maxAttempt;
     private int attemptLeft;
     private final int maxNum;
 
-    public GameEngine(int maxAttempt,int maxNum){
-
+    public GameEngine(int maxAttempt, int maxNum) {
         Random rand = new Random();
-        this.targetNum = rand.nextInt(100)+1;
-        this.maxAttempt  = maxAttempt;
-        this.maxNum = maxNum;         
-
+        this.targetNum = rand.nextInt(maxNum) + 1; // use maxNum, not 100
+        this.maxAttempt = maxAttempt;
+        this.maxNum = maxNum;      
+        this.attemptLeft = maxAttempt; // BUG FIX 1: initialize attempts
     }
 
-    public boolean isOutOfAttempts(){
-        return attemptLeft <=0;
-
+    public boolean isOutOfAttempts() {
+        return attemptLeft <= 0;
     }
 
-    public int getAttemptLeft(){
+    public int getAttemptsLeft() { // BUG FIX 2: match Main.java naming
         return attemptLeft;
     }
 
-
-    public int getTargetNum(){
+    public int getTargetNum() {
         return targetNum;
-
     }
 
+    public int getMaxNum() { // added this for InputHandler
+        return maxNum;
+    }
 
-    public String evaluateGuess(int guess){
-        attemptLeft --;
-        if (guess == targetNum){
-            return "Correct 😎";
-
-        }
-
-        else if(guess > targetNum){
-            return "Too High";
-        }
-
-        else if(guess < targetNum){
-            return "Too low";
-        }
-
-        else{
-            return "Enter a correcct number";
+    public String evaluateGuess(int guess) {
+        attemptLeft--; // only decrease on valid guess
+        if (guess == targetNum) {
+            return "CORRECT"; // BUG FIX 3: ALL CAPS to match switch in Main
+        } else if (guess > targetNum) {
+            return "TOO_HIGH";
+        } else {
+            return "TOO_LOW";
         }
     }
 }
